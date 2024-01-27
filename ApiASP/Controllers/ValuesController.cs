c55ba29de44734ae8422c62b3da4408e1d080a81
Тимощук Саша <timoshchyk@gmail.com>
2024-01-27 12:25:06 +0200

    Добавьте файлы проекта.

---
+++ ApiASP/Controllers/ValuesController.cs
@@ -0,0 +1,36 @@
+﻿using ApiASP.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiASP.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+		private readonly FirstDBContext _context;
+		public ValuesController(FirstDBContext context)
+		{			
+			_context = context;
+		}
+		[HttpGet]
+		public async Task <ActionResult<IEnumerable<User>>> GetUsersList()
+		{
+			return Ok(_context.Users.ToList());
+		}
+		[HttpPost]
+		public async Task<ActionResult<IEnumerable<User>>> Add(User user)
+		{
+			_context.Users.Add(user);
+			_context.SaveChanges();
+			return Ok();
+		}
+		[HttpDelete]
+		public async Task<ActionResult<IEnumerable<User>>> Delete(User user)
+		{
+			_context.Users.Remove(user);
+			_context.SaveChanges();
+			return Ok();
+		}
+	}
+}
---
+++ ApiASP/Models/FirstDBContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ApiASP.Models
+{
+    public partial class FirstDBContext : DbContext
+    {
+        public FirstDBContext()
+        {
+        }
+
+        public FirstDBContext(DbContextOptions<FirstDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<MigrationHistory> MigrationHistories { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MigrationHistory>(entity =>
+            {
+                entity.HasKey(e => new { e.MigrationId, e.ContextKey })
+                    .HasName("PK_dbo.__MigrationHistory");
+
+                entity.ToTable("__MigrationHistory");
+
+                entity.Property(e => e.MigrationId).HasMaxLength(150);
+
+                entity.Property(e => e.ContextKey).HasMaxLength(300);
+
+                entity.Property(e => e.ProductVersion).HasMaxLength(32);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
